fc20092ea930f7452720ba4e7d416cf9e47d3290
Alex Cantu <miguel.can2@gmail.com>
2023-04-16 22:12:32 +0000

    First commit

---
+++ src/components/Home.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="home" class="centerStage">
+    <div
+        class="bg-image text-center"
+        style="
+        background-image: url('https://www.dropbox.com/s/cd4lvp7hqb94sye/IMG_4998.JPG?raw=true');
+        height: 100vh;">
+      <NamePlate />
+      <SocialMediaReel />
+    </div>
+  </div>
+</template>
+
+
+<script>
+import NamePlate from './NamePlate.vue';
+import SocialMediaReel from './SocialMediaReel.vue';
+
+export default {
+  components: {
+    NamePlate,
+    SocialMediaReel
+  },
+  name: 'home',
+  data: function () {
+    return {
+    }
+  },
+  created () { },
+  methods: {
+    async setup () {
+    },
+  }
+}
+</script>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div
+    class="bg-image"
+    style="
+      background-image: url('https://mdbcdn.b-cdn.net/img/Photos/Others/images/76.webp');
+      height: 100vh;">
+    <h1>This is an about page</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
